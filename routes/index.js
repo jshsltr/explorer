d04ad4817e0dd84492771e1f885d5da1b0682d00
jshsltr <jshsltr@gmail.com>
2020-02-08 22:50:06 -0600

    missing requirement

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var express = require('express')
     , locale = require('../lib/locale')
     , db = require('../lib/database')
     , lib = require('../lib/explorer')
+    , formatNum = require('format-num')
     , qr = require('qr-image');
 
 function route_get_block(res, blockhash) {
